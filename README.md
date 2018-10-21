aa38f3b4654f6719b604885b5e2cad07aa86230e
Veniamin Ilmer <any@email.com>
2018-10-21 08:57:26 -0500

    println

---
+++ README.md
@@ -0,0 +1,11 @@
+# CAC Decoder
+
+This code will decode CAC barcodes.
+
+Common Access Cards have two barcodes.
+
+One the front (PDF417), and one the back (Code39).
+
+Find an application that can read a PDF417 barcode, read the CAC's code.
+
+Copy and paste it into here, and this will decode it.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+//This project was inspired by https://github.com/jkusner/CACBarcode/blob/master/cacbarcode.py
+
 fn main() {
-    println!("Hello, world!");
+  if std::env::args().count() > 1 {
+    println!("For security, the barcodes should only be passed via stdin, not as arguments.");
+    std::process::exit(1);
+  }
+
+  println!("Common Access Cards have two barcodes.");
+  println!("One the front (PDF417), and one the back (Code39).");
+  println!("Get an application that can read a PDF417 barcode.");
+  println!("Copy and paste it into here, and I will decode it.");
+  println!("The decoded info will only be presented here, and will not be saved.");
+  println!();
+  
+  use std::io::prelude::*;
+  let stdin = std::io::stdin();
+  for line in stdin.lock().lines() {
+    println!("{}", decode(line.unwrap()));
+  }
+}
+
+fn decode(line: String) -> String {
+  //PDF217
+  "test".to_string()
 }
