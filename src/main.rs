c7e58872521a49e7094c7a8b1e60d72d980616e6
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-10-22 09:32:37 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@
 
 extern crate base_custom;
 use base_custom::BaseCustom;
-/*
-fn main() {
-  println!("{}", base32.decimal("TONMLL"));
-}*/
+
+extern crate chrono;
+use chrono::prelude::*;
+extern crate time;
+use time::Duration;
 
 fn main() {
   if std::env::args().count() > 1 {
@@ -38,6 +39,7 @@ fn decode(data: String) -> String {
   }
 
   let base32 = BaseCustom::<String>::new("0123456789ABCDEFGHIJKLMNOPQRSTUV", None);
+  let base_time = Utc.ymd(1000, 1, 1);
   
   let mut data_chars = data.chars();
 
@@ -75,8 +77,9 @@ fn decode(data: String) -> String {
   //Last Name
   out.push(("Last Name", data_chars.by_ref().take(26).collect::<String>()));
 
-  //Read Date
-  let _date = data_chars.by_ref().take(4).collect::<String>();
+  //Date of Birth
+  let days = data_chars.by_ref().take(4).collect::<String>().parse::<i64>();
+  out.push(("Date of Birth", (base_time + Duration::days(days)).format("%a, %e %b %Y")));
   
   //Personnel Category Code
   let pcc = data_chars.next().unwrap();
@@ -128,9 +131,75 @@ fn decode(data: String) -> String {
   let pect = (data_chars.next().unwrap(), data_chars.next().unwrap())
   out.push(("Personnel Entitlement Condition Type", match pect {
   ('0', '1') => "On Active Duty. Segment condition.",
+  ('0', '2') => "Mobilization. Segment condition.",
+  ('0', '3') => "On appellate leave. Segment condition.",
+  ('0', '4') => "Military prisoner. Segment condition.",
+  ('0', '5') => "POW/MIA. Segment condition.",
+  ('0', '6') => "Separated from Selected Reserve. Event condition.",
+  ('0', '7') => "Declared permanently disabled after temporary disability period. Event condition.",
+  ('0', '8') => "On non-CONUS assignment. Segment condition.",
+  ('0', '9') => "Living in Guam or Puerto Rico. Segment condition.",
+  ('1', '0') => "Living in government quarters. Segment condition.",
+  ('1', '1') => "Death determined to be related to an injury, illness, or disease while on Active duty for training or while traveling to or from a place of duty. Event condition.",
+  ('1', '2') => "Discharged due to misconduct involving family member abuse. (Sponsors who are eligible for retirement.) Segment condition.",
+  ('1', '3') => "Granted retired pay. Event condition.",
+  ('1', '4') => "DoD sponsored in U.S. (foreign military). Segment condition.",
+  ('1', '5') => "DoD non-sponsored in U.S. (foreign military). Segment condition.",
+  ('1', '6') => "DoD sponsored overseas. Segment condition.",
+  ('1', '7') => "Deserter. Segment condition.",
+  ('1', '8') => "Discharged due to misconduct involving family member abuse. (Sponsors who are not eligible for retirement.) Segment condition.",
+  ('1', '9') => "Reservist who dies after receiving their 20 year letter. Event condition.",
+  ('2', '0') => "Transitional assistance (TA-30). Segment condition.",
+  ('2', '1') => "Transitional assistance (TA-Res). Segment condition.",
+  ('2', '2') => "Transitional assistance (TA-60). Segment condition.",
+  ('2', '3') => "Transitional assistance (TA-120). Segment condition.",
+  ('2', '4') => "Transitional assistance (SSB program). Segment condition.",
+  ('2', '5') => "Transitional assistance (VSI program). Segment condition.",
+  ('2', '6') => "Transitional assistance (composite). Segment condition.",
+  ('2', '7') => "Senior Executive Service (SES).",
+  ('2', '8') => "Emergency Essential - overseas only.",
+  ('2', '9') => "Emergency Essential - CONUS.",
+  ('3', '0') => "Emergency Essential - CONUS in living quarters, living on base, and not drawing a basic allowance for quarters, serving in an emergency essential capacity.",
+  ('3', '1') => "Reserve Component TA-120 Reserve Component Transition Assistance TA 120 (Jan 1, 2002 or later).",
+  ('3', '2') => "On MSC owned and operated vessels Deployed to foreign countries on Military Sealift Command owned and operated vessels. Segment condition.",
+  ('3', '3') => "Guard/Reserve Alert Notification Period.",
+  ('3', '4') | ('3', '5') => "Reserve Component TA-180 - 180 days TAMPS for reserve return from named contingencies.",
+  ('3', '6') | ('3', '7') => "TA-180 - 180 days TAMP for involuntary separation.",
+  ('3', '8') => "Living in Government Quarters in Guam or Puerto Rico, Living on base and not drawing an allowance for quarters in Guam or Puerto Rico.",
+  ('3', '9') => "Reserve Component TA-180 - TAMP - Mobilized for Contingency.",
+  ('4', '0') => "TA-180 TAMP - SPD Code Separation.",
+  ('4', '1') => "TA-180 - TAMP - Stop/Loss Separation.",
+  ('4', '2') => "DoD Non-Sponsored Overseas - Foreign Military personnel serving OCONUS not sponsored by DoD.",
   _ => format!("Unknown Type {}", pect),
   }));
 
+  //Rank
+  out.push(("Rank", data_chars.by_ref().take(6).collect::<String>())); 
+
+  //Pay Plan Code
+  out.push(("Pay Plan Code", data_chars.by_ref().take(2).collect::<String>())); 
+
+  //Pay Plan Grade Code
+  out.push(("Pay Plan Grade Code", data_chars.by_ref().take(2).collect::<String>())); 
+
+  //Card Issue Date
+  let days = data_chars.by_ref().take(4).collect::<String>().parse::<i64>();
+  out.push(("Card Issue Date", (base_time + Duration::days(days)).format("%a, %e %b %Y")));
+  
+  //Card Expiration Date
+  let days = data_chars.by_ref().take(4).collect::<String>().parse::<i64>();
+  out.push(("Card Expiration Date", (base_time + Duration::days(days)).format("%a, %e %b %Y")));
+  
+  //Card Instance Identifier (Random)
+  let cii = data_chars.next().unwrap();
+  out.push(("Card Instance Identifier (Random)", cii.to_string());
+
+  if data.len() == 89 {
+    //Middle Initial
+    let initial = data_chars.next().unwrap();
+    out.push(("Middle Initial", initial.to_string());
+  }
+  
   out.iter().map(|(key, val)| format!("{}: {}\n", key, val)).collect::<String>()
   
 }
